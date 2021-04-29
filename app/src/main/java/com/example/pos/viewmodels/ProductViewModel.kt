78569f5634ff696926cb880f45ad9874a2bfb2d6
verybaktiar <hawinvery35@gmail.com>
2021-04-29 15:14:43 +0700

    kurang edit

--- app/src/main/java/com/example/pos/viewmodels/ProductViewModel.kt
+++ app/src/main/java/com/example/pos/viewmodels/ProductViewModel.kt
@@ -16,18 +16,18 @@ class ProductViewModel : ViewModel(){
     fun fetchDummyProduct(){
         //pura2nya dari API
         val dummies = mutableListOf<Product>().apply {
-            add(Product(1, "Whiskas Ocean Fish", 3000, "http://p.ipricegroup.com/uploaded_f10bfa776289954438da7595a7e1a926.jpg"))
-            add(Product(2, "Whiskas Tuna", 3500, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVOsFfGKQ-nYpAc-E-5nx0gI4FViBGEuPf6w&usqp=CAU"))
-            add(Product(3, "Oricat Adult", 3000, "https://ecs7.tokopedia.net/img/cache/500-square/VqbcmM/2020/12/13/5aa47e50-a3e4-4608-a244-e8d2e4abd293.jpg"))
-            add(Product(4, "Oricat Kitten", 8000, "https://id-test-11.slatic.net/p/8218ebd1a06e1ab9bdb8e36444c02215.jpg_720x720q80.jpg_.webp"))
-            add(Product(5, "Royal Cannin Mother & Baby", 6000, "https://id-test-11.slatic.net/p/29378f4cf021061087638b400d075e8a.jpg"))
-            add(Product(6, "Royal Cannin Kitten", 6000, "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/MTA-2905145/royal-canin_royal-canin-kitten-makanan-anak-kucing--dry--2-kg-_full09.jpg"))
-            add(Product(7, "Royal Cannin Persia", 6000, "https://cf.shopee.co.id/file/e3f87c099a85ef06927f4f7217d51a30"))
-            add(Product(8, "Snack Me-O", 6000, "https://cf.shopee.co.id/file/dae243e2c202f3cf9f38d32e714db557"))
-            add(Product(9, "Kandang", 6000, "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//93/MTA-2554409/petshop-murah_petshop-murah-k02-kandang-lipat--tanpa-roda-_full03.jpg"))
-            add(Product(10, "Vitamin Hewan", 6000, "https://s1.bukalapak.com/img/14072370971/large/data.png"))
-            add(Product(11, "Litter Box", 6000, "https://assets.petco.com/petco/image/upload/f_auto,q_auto/2535922-center-7"))
-            add(Product(12, "Pasir", 6000, "https://s0.bukalapak.com/img/0409522402/large/Pasir_kucing_Kawan_Cat_Litter_20kg___Pasir_gumpal_wangi.png"))
+            add(Product(1, "Whiskas Ocean Fish", 299900, "http://p.ipricegroup.com/uploaded_f10bfa776289954438da7595a7e1a926.jpg"))
+            add(Product(2, "Whiskas Tuna", 449850, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVOsFfGKQ-nYpAc-E-5nx0gI4FViBGEuPf6w&usqp=CAU"))
+            add(Product(3, "Oricat Adult", 22000, "https://ecs7.tokopedia.net/img/cache/500-square/VqbcmM/2020/12/13/5aa47e50-a3e4-4608-a244-e8d2e4abd293.jpg"))
+            add(Product(4, "Oricat Kitten", 20600, "https://id-test-11.slatic.net/p/8218ebd1a06e1ab9bdb8e36444c02215.jpg_720x720q80.jpg_.webp"))
+            add(Product(5, "Royal Cannin Mother & Baby", 303000, "https://id-test-11.slatic.net/p/29378f4cf021061087638b400d075e8a.jpg"))
+            add(Product(6, "Royal Cannin Kitten", 230000, "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/MTA-2905145/royal-canin_royal-canin-kitten-makanan-anak-kucing--dry--2-kg-_full09.jpg"))
+            add(Product(7, "Royal Cannin Persia", 455100, "https://cf.shopee.co.id/file/e3f87c099a85ef06927f4f7217d51a30"))
+            add(Product(8, "Snack Me-O", 19499, "https://cf.shopee.co.id/file/dae243e2c202f3cf9f38d32e714db557"))
+            add(Product(9, "Kandang", 425000, "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//93/MTA-2554409/petshop-murah_petshop-murah-k02-kandang-lipat--tanpa-roda-_full03.jpg"))
+            add(Product(10, "Vitamin Hewan", 40000, "https://s1.bukalapak.com/img/14072370971/large/data.png"))
+            add(Product(11, "Litter Box", 22750, "https://assets.petco.com/petco/image/upload/f_auto,q_auto/2535922-center-7"))
+            add(Product(12, "Pasir", 105000, "https://s0.bukalapak.com/img/0409522402/large/Pasir_kucing_Kawan_Cat_Litter_20kg___Pasir_gumpal_wangi.png"))
         }
         products.postValue(dummies)
 
